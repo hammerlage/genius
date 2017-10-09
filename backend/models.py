e808ef732b19a2f92f80adfce3dc5612a8ba753a
Arthur Vinicius <arthurvs@ciandt.com>
2017-10-09 17:20:57 -0300

    Ajustes na API backend em python

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,8 @@
 """Genius backend API - Models"""
 
 from google.appengine.ext import ndb
+import json
+import make_json_serializable
 
 class JsonResponse(object):
   success = True
@@ -12,7 +14,14 @@ class JsonResponse(object):
     if data is None:
         self.success = False
     else:
-      self.data = data
+      self.data = json.dumps(data)
+
+  def to_json(self):
+    if data is None:
+      return "{'success': '%s'" % self.success.decode('utf-8')
+    else:
+      return "{'success': '%s', 'data': '%s'}" % (self.success.decode('utf-8'),
+        self.data.decode('utf-8'))
 
 class Click(ndb.Model):
 
