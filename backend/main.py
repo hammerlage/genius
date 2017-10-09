1602191e60fe9781fb4f831f0d78a03c37715040
Arthur Vinicius <arthurvs@ciandt.com>
2017-10-09 14:20:14 -0300

    Ajustes na API backend em python

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,13 @@
-
+#!/usr/bin/env python
 """Genius backend API"""
 
 # [START imports]
-import endpoints
-from protorpc import message_types
-from protorpc import messages
-from protorpc import remote
+from handlers import *
+import webapp2
 # [END imports]
 
-
-# [START messages]
-class Click(messages.Message):
-    button = messages.StringField(1)
-    timestamp = messages.StringField(1)
-# [END messages]
-
-
-# [START click_api]
-@endpoints.api(name='click', version='v1')
-class ClickApi(remote.Service):
-
-    @endpoints.method(
-        message_types.VoidMessage,
-        Click,
-        path='register',
-        http_method='POST',
-        name='register')
-    def register(self, request):
-        return Click(button=request.button, timestamp=request.timestamp)
-
-
-# [START api_server]
-api = endpoints.api_server([ClickApi])
-# [END api_server]
+application = webapp2.WSGIApplication(
+    [('/', IndexHandler),
+     ('/api/clicks', ClicksHandler)
+    ],
+    debug=False)
