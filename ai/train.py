6e049a649285611c7af86aa9fa3e21ae90fa49a9
Arthur <arthurnops@gmail.com>
2017-10-18 11:37:00 -0200

    Tratamento de rounds

--- ai/train.py
+++ ai/train.py
@@ -4,8 +4,12 @@ import sys
 import csv
 import repeat as rp
 
+GAME_ID = 0
+ROUND_CHALLENGE = 3
+CLICK_TIMESTAMP = 8
+
 def is_valid(sequence):
-    if len(sequence) > 2:
+    if len(sequence) > 3:
         return True
     return False
 
@@ -13,6 +17,23 @@ def bool_to_int(string):
     if string == 'True':
         return 1
     return 0
+
+def round(row, roundRows):
+    result = None
+
+    gameId,userName,roundSuccess,roundChallenge,currClickAnswer,roundStartAt,roundStartTimestamp,clickTimeAt,clickTimestamp,totalRoundChallenge,roundColors,totalRoundColors = row 
+
+    sequence = rp.replace(roundChallenge)
+
+    if is_valid(sequence):
+
+        a = []
+        for r in roundRows:
+            a.append(float(r[CLICK_TIMESTAMP]))
+
+        result = [sequence, bool_to_int(roundSuccess), totalRoundChallenge, totalRoundColors]
+
+    return result
 
 def train(input_csv_file, output_csv_file):
 
@@ -23,21 +44,18 @@ def train(input_csv_file, output_csv_file):
         writer = csv.writer(open(output_csv_file, 'w'))
         writer.writerow(title)
         oldRow = None
+        roundRows = []
         for row in reader:
-            gameId,userName,roundSuccess,roundChallenge,currClickAnswer,roundStartAt,roundStartTimestamp,clickTimeAt,clickTimestamp,totalRoundChallenge,roundColors,totalRoundColors = row 
+            if oldRow and (row[GAME_ID] != oldRow[GAME_ID] or row[ROUND_CHALLENGE] != oldRow[ROUND_CHALLENGE]):
+                roundResult = round(row, roundRows)
+                if roundResult:
+                    writer.writerow(roundResult)
 
-            sequence = rp.replace(roundChallenge)
-
-            if is_valid(sequence):
-
-                repetitions = rp.find(sequence, 0)
-
-                result = [sequence, bool_to_int(roundSuccess), totalRoundChallenge, totalRoundColors]
-                        
-                writer.writerow(result)
-
+                roundRows = []
+                result = []
+            
+            roundRows.append(row)
             oldRow = row
-
 
 def main():
     if len(sys.argv) != 3:
